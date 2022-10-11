3ef17c1d62799c9cc9ec30740190bcb4e2ed9e85
Yikang1020 <liuyk11@126.com>
2022-10-11 20:42:43 +0800

    line

---
+++ line.R
@@ -0,0 +1,42 @@
+library(ggplot2)
+library(tidyverse)
+library(reshape2)
+
+no_cue = c(570,660,800,1100,790,650)
+cue_upright_inverted = c(530,580,650,970,720,585)
+cue_slope = c(480,530,650,890,710,550)
+cue_u_s = c(400,410,430,430,415,410)
+angle = c('0 or 360','60','120','180','240','300')
+
+df = data.frame(no_cue,cue_upright_inverted,cue_slope,cue_u_s,angle)
+
+df = melt(df, id="angle",value.name = 'value')  
+
+df = data.frame(df)
+df$value = as.numeric(df$value)
+
+p = ggplot(data = df,aes(x = angle, y = value, group = factor(variable), color = variable)) +
+#  geom_col(fill = 'black',width = .5)+
+  geom_line()+
+  geom_smooth(se=FALSE, linetype="dashed", size=0.5) +
+#  geom_xspline(size=0.5)+
+  geom_point(size=3)+
+  theme_bw()+
+#  theme_classic()+
+#  theme(axis.text.x = element_text(face="bold", size=12),axis.text.y = element_text(face="bold", size=12))+
+#  scale_y_continuous(expand=c(0,0), limits=c(0, 35), breaks=seq(0, 35, by=5),sec.axis = sec_axis(~.+5,name="Number"))+
+#  geom_text(aes(Var1,prob+2,label = label),size = 4,fontface = "bold",hjust = 0.5,vjust = 1)+  
+#  theme(axis.line.x=element_line(size=.5, colour="black"),
+#        axis.line.y=element_line(size=.5, colour="black"),
+#        axis.title.x=element_text(colour='black', size=16,face = "bold"),
+#        axis.title.y=element_text(colour='black', size=16,face = "bold"),
+#        axis.ticks = element_line(color = "black"),
+#        axis.ticks.length = unit(0.2,"lines"), #控制坐标轴深处标签
+#        axis.line = element_line(colour = "black",size = 14), 
+#        axis.text.y = element_text(colour='black',size=10),
+#        panel.border = element_blank(),
+#        panel.background = element_blank(),
+#        axis.text.x = element_text(angle = 45,colour = "black",size = 10,vjust = 0.8,hjust = 0.8))+
+  ggtitle("spline_shape = 0")+
+  labs(x=NULL,y=NULL)
+
